44e962433677d07a41e744bdf4f40c00aa9cf8c7
DFBDev <browndon962@gmail.com>
2024-04-02 15:33:06 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        int iterationCounter = 0;
+        int rgbSum = 0;
+        try (FrameGrabber fg = new OpenCVFrameGrabber(0)){ //Instantiating frame-grabber with primary camera in try-with-resources statement.
+            //Starting frame grabber, creating instance of frame converter (to convert to buffered image), and creating instance of JFrame container.
+            fg.start();
+            Java2DFrameConverter j2d = new Java2DFrameConverter();
+            JFrame jf = new JFrame("ASCIICam");
+
+           while(true) {
+               ArrayList<Integer> rgbValArray = new ArrayList<>(); //Storage for sequential collection of RGB values, which is later passed to customJPanel class.
+
+               //Grabbing frame of video and converting it to buffered image; which offers more functionalities.
+               Frame frame = fg.grabFrame();
+               BufferedImage bi = j2d.getBufferedImage(frame);
+
+               //Loop for processing pixels of image/video frame.
+               //Outer for-loop is responsible for transversing the image horizontally, and moving forward 10 pixels after each iteration; ultimately treats the image as a collection of 10x10 pixel columns.
+               //Nested for-loop is responsible for transversing vertically on each column and capturing 10x10 pixel sub-image.
+               for (int k = 0; k < bi.getWidth(); k = k + 10) {
+                   for (int r = 0; r < bi.getHeight(); r = r + 10){
+                       BufferedImage subimage = bi.getSubimage(k,r, 10,10); //10x10 pixel sub-image.
+                       for (int i = 0; i < subimage.getWidth(); i++) { //Calculating average RGB value for sub-image.
+                           for (int j = 0; j < subimage.getHeight(); j++){
+                               Color rgbValues = new Color(subimage.getRGB(i, j));
+                               rgbSum = rgbSum + rgbValues.getRed() + rgbValues.getBlue() + rgbValues.getGreen();
+                               iterationCounter++; //Used as divisor for rgb value total later on to calculate the average.
+                           };
+                       };
+                       rgbValArray.add(rgbSum / (iterationCounter * 3));
+                       //Resetting values.
+                       rgbSum = 0;
+                       iterationCounter = 0;
+                   };
+               };
+                //Creating JPanel, setting properties of JFrame container, adding JPanel to JFrame container.
+               JPanel jp = new customJPanel(rgbValArray);
+               jf.setVisible(true);
+               jf.setSize(1600,1500);
+               jf.add(jp);
+           }
+        }
+        catch (FrameGrabber.Exception e) {
+            System.out.print("PROGRAM ERROR: " + e);
+        };
+    }
+}
---
+++ src/main/java/org/example/customJPanel.java
@@ -0,0 +1,116 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+
+public class customJPanel extends JPanel {
+    int counter = 0; //Counter for accessing data in-order from rgbArray.
+    ArrayList<Integer> rgbArray;
+    customJPanel(ArrayList<Integer> rgbValueArray) { //Setting constructor to accept rgbValueArray dependency from Main, and overriding default JPanel background color.
+        this.setBackground(Color.BLACK);
+        rgbArray = rgbValueArray;
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.WHITE);
+        //Painting ASCII symbols on JPanel in the same order that sub-images from Main were processed.
+        //The amount of symbols painted on JPanel matches the sub-images collected from the original image/frame.
+        //The RGB averages are passed from Main are processed in-order. An ASCII symbol is painted depending on the current average being processed.
+        for (int i = 0; i < 1280; i = i + 20) {
+            for (int j = 0; j < 960; j = j + 20){
+                if (rgbArray.get(counter) > 0 && rgbArray.get(counter) <= 34){
+                    g.drawString(" ", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 35 && rgbArray.get(counter) <= 39) {
+                    g.drawString("_", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 40 && rgbArray.get(counter) <= 44) {
+                    g.drawString(".", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 45 && rgbArray.get(counter) <= 49) {
+                    g.drawString(",", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 50 && rgbArray.get(counter) <= 54) {
+                    g.drawString("-", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 55 && rgbArray.get(counter) <= 59) {
+                    g.drawString("-", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 60 && rgbArray.get(counter) <= 64) {
+                    g.drawString("=", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 65 && rgbArray.get(counter) <= 69) {
+                    g.drawString("+", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 70 && rgbArray.get(counter) <= 79) {
+                    g.drawString(":", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 80 && rgbArray.get(counter) <= 89) {
+                    g.drawString(";", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 90 && rgbArray.get(counter) <= 99) {
+                    g.drawString("c", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 100 && rgbArray.get(counter) <= 109) {
+                    g.drawString("b", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 110 && rgbArray.get(counter) <= 119) {
+                    g.drawString("a", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 120 && rgbArray.get(counter) <= 129) {
+                    g.drawString("!", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 130 && rgbArray.get(counter) <= 139) {
+                    g.drawString("?", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 140 && rgbArray.get(counter) <= 149) {
+                    g.drawString("0", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 150 && rgbArray.get(counter) <= 159) {
+                    g.drawString("1", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 160 && rgbArray.get(counter) <= 169) {
+                    g.drawString("2", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 170 && rgbArray.get(counter) <= 179) {
+                    g.drawString("3", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 180 && rgbArray.get(counter) <= 189) {
+                    g.drawString("4", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 190 && rgbArray.get(counter) <= 199) {
+                    g.drawString("5", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 200 && rgbArray.get(counter) <= 209) {
+                    g.drawString("6", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 210 && rgbArray.get(counter) <= 219) {
+                    g.drawString("7", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 220 && rgbArray.get(counter) <= 229) {
+                    g.drawString("8", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 230 && rgbArray.get(counter) <= 234) {
+                    g.drawString("9", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 235 && rgbArray.get(counter) <= 239) {
+                    g.drawString("$", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 240 && rgbArray.get(counter) <= 244) {
+                    g.drawString("W", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 245 && rgbArray.get(counter) <= 249) {
+                    g.drawString("#", i + 150, j + 200);
+                }
+                else if (rgbArray.get(counter) >= 250 && rgbArray.get(counter) <= 255) {
+                    g.drawString("@", i + 150, j + 200);
+                }
+                counter += 1;
+            }
+        };
+        //Resetting counter.
+        counter = 0;
+    }
+}
